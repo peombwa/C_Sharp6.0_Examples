fb2053f9e0fff74639ee4c75c35fae075f27f69f
pwombwa <pwombwa@live.com>
2016-11-22 13:57:15 +0000

    Added adcanced operators

---
+++ C_Sharp6.0_Example/AdvancedOperators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            Console.WriteLine($"x is null: {x}");
+
+            //Null conditional operator
+            List<string> authors = null;
+            int? count = authors?.Count;
+
+            //Ternary operator
+            string message = count == null ? "Count is null " : "Count is not null" ;
+            Console.WriteLine(message);
+
+            //Null coalescing
+            int howMany = authors?.Count ?? 0;
+            Console.WriteLine($"howmany: {howMany}");
+
+            authors = new List<string>
+            {
+                "author one",
+                "author two"
+            };
+
+            howMany = authors?.Count ?? 0;
+            Console.WriteLine($"howmany = {howMany}");
+
+            Console.ReadLine();
+        }
+    }
+}
